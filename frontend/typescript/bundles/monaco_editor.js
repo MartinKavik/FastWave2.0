3368167406e4182abe17a442980ac88b541a3ede
Martin Kavík <martin@kavik.cz>
2024-12-12 17:36:31 +0100

    monaco_editor.ts

---
+++ frontend/typescript/bundles/monaco_editor.js
@@ -0,0 +1,1 @@
+var e=class{constructor(){}async init(t){console.log("INIT MonacoEditorController!")}};export{e as MonacoEditorController};
---
+++ frontend/typescript/monaco_editor/monaco_editor.ts
@@ -0,0 +1,7 @@
+export class MonacoEditorController {
+    constructor() {}
+
+    async init(parent_element: HTMLElement) {
+        console.log("INIT MonacoEditorController!")
+    }
+}
