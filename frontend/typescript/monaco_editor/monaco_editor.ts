ad7d61731d173bdae020c8f13c1cd82a96bd774b
Martin Kavík <martin@kavik.cz>
2024-12-12 20:48:31 +0100

    basic code editor

--- frontend/typescript/monaco_editor/monaco_editor.ts
+++ frontend/typescript/monaco_editor/monaco_editor.ts
@@ -1,7 +1,24 @@
+import './style.css';
+import * as monaco from 'monaco-editor';
+// import { initialize as initializeEditorWorker } from 'monaco-editor/esm/vs/editor/editor.worker';
+
 export class MonacoEditorController {
     constructor() {}
 
     async init(parent_element: HTMLElement) {
-        console.log("INIT MonacoEditorController!")
+        // window.MonacoEnvironment = {
+        //     getWorker
+        //     getWorkerUrl: function (moduleId, label) {
+        //         return './vs/editor/editor.worker.js';
+        //     }
+        //     // getWorker(_workerId: any, _label: string) {
+        //     //     const worker = new initializeEditorWorker();
+        //     //     console.log(worker);
+        //     //     return worker;
+        //     // }
+        // };
+        monaco.editor.create(parent_element, {
+            value: "Hello world!"
+        });
     }
 }
